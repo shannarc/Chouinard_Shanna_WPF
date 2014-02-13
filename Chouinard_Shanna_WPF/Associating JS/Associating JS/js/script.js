232f2e9879b565ec3dd64874268930a9c3c6d2f4
shanna chouinard <shannarc@gmail.com>
2014-02-13 14:05:43 -0500

    Console.log and Alert

--- Chouinard_Shanna_WPF/Associating JS/Associating JS/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Associating JS/js/script.js
@@ -1,7 +1,10 @@
-/**
- * Created with JetBrains WebStorm.
- * User: ANewLife
- * Date: 2/13/14
- * Time: 1:26 PM
- * To change this template use File | Settings | File Templates.
- */
+//JavaScript
+
+var yearBorn = 1955;
+
+/*
+console.log("Hello World");
+console.log(yearBorn);
+*/
+
+alert(yearBorn);
