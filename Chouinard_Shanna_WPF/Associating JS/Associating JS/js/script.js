396aa4e61035315aa4558f2711d3c36b2ec13d32
shanna chouinard <shannarc@gmail.com>
2014-02-13 20:15:47 -0500

    Assignment Operators

--- Chouinard_Shanna_WPF/Associating JS/Associating JS/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Associating JS/js/script.js
@@ -1,16 +1,8 @@
-//Casting
+//Expressions - Assignment Operators
 
-var stringVar = "6";
-var result = 7 + Number(stringVar);
-console.log(result);
-
-var areaCode = 407;
-var firstPart = 203;
-var secPart = 4529;
- //(407) 203-4529
-var phoneNo = "(" + String(areaCode) + ")" + String(firstPart) + "-" + String(secPart);
-
-console.log(phoneNo);
+var a = 3;
+a ++ ;
+console.log(a);
 
 
 
