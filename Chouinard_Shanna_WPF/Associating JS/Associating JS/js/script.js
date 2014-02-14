2de018c77de564dc49f5750b9704db623e54c8cb
shanna chouinard <shannarc@gmail.com>
2014-02-13 19:17:19 -0500

    Expressions Arithmetic Operators

--- Chouinard_Shanna_WPF/Associating JS/Associating JS/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Associating JS/js/script.js
@@ -1,6 +1,7 @@
-//Expressions  Age Example
+//Expressions  Arithmetic Operators
 
-var yearBorn = 1978;
-var age = 2014 - yearBorn;
-console.log(age);
-
+//area of the triangle is half of the width times the height
+var width = 4;
+var height = 5;
+var area = width * height/2;
+console.log (area);
