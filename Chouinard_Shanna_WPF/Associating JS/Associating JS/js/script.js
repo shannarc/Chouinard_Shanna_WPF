4127ba5aaf0fe465fb9b6417b3176f5a0a19304c
shanna chouinard <shannarc@gmail.com>
2014-02-13 20:00:57 -0500

    Casting

--- Chouinard_Shanna_WPF/Associating JS/Associating JS/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Associating JS/js/script.js
@@ -1,11 +1,16 @@
-//Expressions - Concatenating Strings
+//Casting
 
-var a = "6";
-var b = "7";
-var result = a + b;
-
+var stringVar = "6";
+var result = 7 + Number(stringVar);
 console.log(result);
 
+var areaCode = 407;
+var firstPart = 203;
+var secPart = 4529;
+ //(407) 203-4529
+var phoneNo = "(" + String(areaCode) + ")" + String(firstPart) + "-" + String(secPart);
+
+console.log(phoneNo);
 
 
 
