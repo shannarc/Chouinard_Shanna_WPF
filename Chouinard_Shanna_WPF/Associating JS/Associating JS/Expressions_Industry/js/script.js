ef7af1ff1fbf1b0e8f484ce1b79d32819480425b
shanna chouinard <shannarc@gmail.com>
2014-02-19 23:15:06 -0500

    Committing Webstorms files

--- Chouinard_Shanna_WPF/Associating JS/Associating JS/Expressions_Industry/js/script.js
+++
@@ -1,4 +0,0 @@
-//Shanna Chouinard  13 February 2014  Expressions Industry
-
-//The area of the office at work is the length times the width
-
---
+++ Chouinard_Shanna_WPF/Associating JS/Expressions_Industry/js/script.js
@@ -0,0 +1,10 @@
+//Shanna Chouinard  13 February 2014  Expressions Industry
+
+//There are two rooms at work with the same area.
+//The total area is the length times the width times two.
+
+var length =25 feet;
+var width =15 feet;
+var area = length * width * 2;
+
+console.log(area);
