2bc44aaf7d17491175e17c15fbe44c5a43207afe
shanna chouinard <shannarc@gmail.com>
2014-02-21 04:17:39 -0500

    Prompt and Alert (area of a rectangle)

--- Chouinard_Shanna_WPF/Associating JS/Associating JS/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Associating JS/js/script.js
@@ -1,8 +1,10 @@
-//Expressions - Assignment Operators
+//Prompt and Alert
 
-var a = 3;
-a ++ ;
-console.log(a);
+
+var width = prompt("Calculating the area of a rectangle. \nPlease enter the width");
+var height = prompt ("Please enter heigth");
+var area = width * height;
+console.log(area);
 
 
 
