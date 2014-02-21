e866445e91bf05fd61fd2ae08bb1a314e356c3ab
shanna chouinard <shannarc@gmail.com>
2014-02-21 03:29:12 -0500

    Expressions Worksheet (Industry)

--- Chouinard_Shanna_WPF/Associating JS/Expressions_Industry/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Expressions_Industry/js/script.js
@@ -1,10 +1,10 @@
-//Shanna Chouinard  13 February 2014  Expressions Industry
+//Shanna Chouinard  21 February 2014 Expressions Worksheet (Industry)
 
-//There are two rooms at work with the same area.
-//The total area is the length times the width times two.
 
-var length =25 feet;
-var width =15 feet;
-var area = length * width * 2;
+//There are two offices at work with the same area.
+//Total area of the two rooms is the length times the width
 
+var length = 22;
+var width = 17;
+var area = length * width * 2
 console.log(area);
