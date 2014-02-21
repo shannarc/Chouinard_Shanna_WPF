bff02f04056c2d94d11df4a3f3d8102705e4f795
shanna chouinard <shannarc@gmail.com>
2014-02-21 04:32:15 -0500

    Prompt and Alert (var result)

--- Chouinard_Shanna_WPF/Associating JS/Associating JS/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Associating JS/js/script.js
@@ -4,7 +4,10 @@
 var width = prompt("Calculating the area of a rectangle. \nPlease enter the width");
 var height = prompt ("Please enter heigth");
 var area = width * height;
-console.log(area);
+//console.log(area); //prints it out to console
+
+var result = "The area of your rectangle is "  + area +  " square feet";
+alert (result);
 
 
 
