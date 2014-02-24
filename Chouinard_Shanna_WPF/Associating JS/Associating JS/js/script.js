16e0f92516934966970df523bbf0e79c0352d1d3
shanna chouinard <shannarc@gmail.com>
2014-02-23 23:19:11 -0500

    Conditional Logic w/an Expression (<=)

--- Chouinard_Shanna_WPF/Associating JS/Associating JS/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Associating JS/js/script.js
@@ -1,14 +1,19 @@
-//Conditional Logic with an expression
+//Conditional Logic - With an Expression
 
 
-var kidHeight = 50;
+var kidHeight = 47;
 var minHeight = 48;
-var sneakerLift = 2;
+
+
+
 //if the child is old enough, print to the console "you can ride!"
 //if the kid os over 48 inches in height
-if(kidHeight + sneakerLift > minHeight){
+if(kidHeight > minHeight){
      //code performed if condition is true
-     console.log("you can ride the coaster!");
+     console.log("You can ride the coaster!");
+}
+if(kidHeight <= minHeight){
+    console.log("Sorry kid, you've got some growing to do first!");
 }
 
 
