35c7f0753ff844a86fb5b47697b488bf74bf1969
shanna chouinard <shannarc@gmail.com>
2014-02-24 00:17:34 -0500

    Conditional Logic - Logical Operators (||)

--- Chouinard_Shanna_WPF/Associating JS/Associating JS/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Associating JS/js/script.js
@@ -1,11 +1,11 @@
 //Conditional Logic - Logical Operators
 
-var budget = 300;
+var budget = 100;
 var iPhonePrice = 199.99;
-var payCheck = 200;
+var wonLottery = true;
 
 //if the price of the phone iss less our budget and if our paycheck is over 300
-if(iPhonePrice < budget && payCheck > 300){
+if(iPhonePrice < budget || wonLottery === true){
    console.log("We can buy the phone!");
 }else{
      console.log("No phone for you!");
