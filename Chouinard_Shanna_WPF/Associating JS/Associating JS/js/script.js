a68f8ae0a9842b0f2132a9bb4518e5de6298e5eb
shanna chouinard <shannarc@gmail.com>
2014-02-24 00:35:23 -0500

    Ternary Operators (much more simple)

--- Chouinard_Shanna_WPF/Associating JS/Associating JS/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Associating JS/js/script.js
@@ -1,14 +1,10 @@
 //Conditional Logic - Ternary Operators
 
-var age = 11;
+var age = 6;
 var book;
 
 //If the child is under 10, they get Green Eggs and Ham otherwise they get the Time Machine
-if(age <10);{
-    book = "Green Eggs and Ham";
-}else{
-    book = "The Time Machine";
-}
+book = (age < 10) ? "Green Eggs and Ham" : "The Time Machine";
 console.log(book);
 
 
