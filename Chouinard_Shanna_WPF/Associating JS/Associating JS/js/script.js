1325a17f640b21de2519837842878c0bc08f6264
shanna chouinard <shannarc@gmail.com>
2014-02-23 23:21:16 -0500

    Conditional Logic w/an Expression (else)

--- Chouinard_Shanna_WPF/Associating JS/Associating JS/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Associating JS/js/script.js
@@ -11,8 +11,8 @@ var minHeight = 48;
 if(kidHeight > minHeight){
      //code performed if condition is true
      console.log("You can ride the coaster!");
-}
-if(kidHeight <= minHeight){
+}else{
+    //code performed if condition is false
     console.log("Sorry kid, you've got some growing to do first!");
 }
 
