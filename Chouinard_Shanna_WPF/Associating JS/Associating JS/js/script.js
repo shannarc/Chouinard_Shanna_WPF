258688a130e8f7b78272b647f4b04e3f05192e87
shanna chouinard <shannarc@gmail.com>
2014-02-23 22:54:54 -0500

    Conditional Logic (False)

--- Chouinard_Shanna_WPF/Associating JS/Associating JS/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Associating JS/js/script.js
@@ -1,14 +1,14 @@
 //Conditional Logic
 
 
-var oldEnough = true;
+var oldEnough = false;
 
 //if the child is old enough, print to the console "you can ride!"
 if(oldEnough){
      //code performed if condition is true
      console.log("you can ride the coaster!");
 }
-
+console.log("what comes after");
 
 
 
