c833266d1d5deb40ad5a239e1af9e4e2b5cdc705
shanna chouinard <shannarc@gmail.com>
2014-02-24 14:27:36 -0500

    Conditional Worksheet (Group 2)

--- Chouinard_Shanna_WPF/Associating JS/Conditional_Worksheet/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Conditional_Worksheet/js/script.js
@@ -1,22 +1,11 @@
-//Conditional Worksheet   Group 1 Expressions with Conditionals
+//Conditional Worksheet   Group 2 Multiple Results
 
 
-var tankHolds = 17;   //Tank holds 17 gallons
-var mpg = 15;         //15 miles per gallon
-var gasInTank = .25;  //25% gas left in tank
 
-if(gasInTank > .75){
-    //If it's more than .75
-    console.log("Yes you can make it without stopping for gas");
-}else if(mpg * gasInTank > 200){
-    //If mpg times gas in the tank is greater than 200
-    console.log("Yes you can make it without stopping for gas");
-}else{
-    //If it's less than .75
-    console.log("You only have x gallons of gas in your tank, better get gas now while you can");
+var grade = 90
+
+if(grade = 90){
+    //If grade is greater than 90
+    console.log("You have a 90%, which means you have earned an A in the class!");
 }
-
-
-
-
 
