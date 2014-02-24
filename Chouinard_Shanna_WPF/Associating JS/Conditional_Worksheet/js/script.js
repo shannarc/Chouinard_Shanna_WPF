5b5d1782db950afebcb45a64cc9c4d5340f26295
shanna chouinard <shannarc@gmail.com>
2014-02-24 14:16:42 -0500

    Expressions Worksheet (Group 1)

--- Chouinard_Shanna_WPF/Associating JS/Conditional_Worksheet/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Conditional_Worksheet/js/script.js
@@ -1,7 +1,22 @@
-/**
- * Created with JetBrains WebStorm.
- * User: ANewLife
- * Date: 2/24/14
- * Time: 1:40 PM
- * To change this template use File | Settings | File Templates.
- */
+//Conditional Worksheet   Group 1 Expressions with Conditionals
+
+
+var tankHolds = 17;   //Tank holds 17 gallons
+var mpg = 15;         //15 miles per gallon
+var gasInTank = .25;  //25% gas left in tank
+
+if(gasInTank > .75){
+    //If it's more than .75
+    console.log("Yes you can make it without stopping for gas");
+}else if(mpg * gasInTank > 200){
+    //If mpg times gas in the tank is greater than 200
+    console.log("Yes you can make it without stopping for gas");
+}else{
+    //If it's less than .75
+    console.log("You only have x gallons of gas in your tank, better get gas now while you can");
+}
+
+
+
+
+
