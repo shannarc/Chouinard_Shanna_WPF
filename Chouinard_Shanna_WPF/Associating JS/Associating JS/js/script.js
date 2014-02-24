bfde66911c28ce8296b0b8066d75da628d32af60
shanna chouinard <shannarc@gmail.com>
2014-02-24 00:01:57 -0500

    Conditional Logic - Logical Operators (No phone for you)

--- Chouinard_Shanna_WPF/Associating JS/Associating JS/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Associating JS/js/script.js
@@ -1,23 +1,17 @@
-//Conditional Logic - Else If
+//Conditional Logic - Logical Operators
 
+var budget = 300;
+var iPhonePrice = 199.99;
+var payCheck = 200;
 
-var kidHeight = 52;
-var minHeight = 48;
-var parentHeight = 45; //the height of the kid with the parent
-
-
-//if the child is old enough, print to the console "you can ride!"
-//if the kid os over 48 inches in height
-if(kidHeight > minHeight){
-     //you can ride
-     console.log("You can ride the coaster!");
-}else if( kidHeight > parentHeight){
-    //you can ride with a parent present
-    console.log("You can ride,but only with a parent present!");
+//if the price of the phone iss less our budget and if our paycheck is over 300
+if(iPhonePrice < budget && payCheck > 300){
+   console.log("We can buy the phone!");
 }else{
-    //sorry you have growing to do
-    console.log("Sorry kid, you've got some growing to do first!");
+     console.log("No phone for you!");
 }
+
+
 
 
 
