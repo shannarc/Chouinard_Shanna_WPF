ba45a5b5439d11cce2d6451f13c4915464cd2925
shanna chouinard <shannarc@gmail.com>
2014-02-25 03:48:55 -0500

    Conditionals Industry (givens)

--- Chouinard_Shanna_WPF/Associating JS/Conditionals_Industry/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Conditionals_Industry/js/script.js
@@ -3,7 +3,8 @@
 
 //If you have 30 weapons to clean, it will take 60 hours to clean all the weapons. It takes 2 hours to clean each weapon.
 
-
+//given number of weapons
+//given number of hours to clean each weapon
 
 
 
