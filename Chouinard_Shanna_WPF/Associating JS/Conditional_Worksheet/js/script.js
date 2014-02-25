59d4713a8618de65b8dc3fc18f7a059a9ca6092c
shanna chouinard <shannarc@gmail.com>
2014-02-25 03:33:34 -0500

    Expressions Worksheet (Group 3)

--- Chouinard_Shanna_WPF/Associating JS/Conditional_Worksheet/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Conditional_Worksheet/js/script.js
@@ -1,11 +1,18 @@
-//Conditional Worksheet   Group 2 Multiple Results
+//Conditional Worksheet   Group 3 Multiple Conditions
 
 
+var movieTime = 7;
+var cusAge = 16;
 
-var grade = 90
-
-if(grade = 90){
-    //If grade is greater than 90
-    console.log("You have a 90%, which means you have earned an A in the class!");
+if(cusAge > 55 || cusAge < 10){
+    //if you age is above 55 or below 10 your ticket price is $7
+    console.log("Your ticket price is $7");
+}elseif(movieTime == 3, 4, 5){
+    //if movie time is 3, 4, or 5 the ticket price is $7
+    console.log("Your ticket price is $7");
+}else{
+    //your ticket price is $12
+    console.log("Your ticket price is $12");
 }
 
+    //I don't know what I am doing wrong on this one.....Please help
