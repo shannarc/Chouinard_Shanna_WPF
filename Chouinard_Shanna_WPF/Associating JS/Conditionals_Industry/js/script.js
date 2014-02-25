40d246e460cc68db2c8cdd80e8b424bf63592873
shanna chouinard <shannarc@gmail.com>
2014-02-25 04:15:44 -0500

    Conditional Industry (changes in the problem)

--- Chouinard_Shanna_WPF/Associating JS/Conditionals_Industry/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Conditionals_Industry/js/script.js
@@ -1,7 +1,7 @@
 //Shanna Chouinard   25 February 2014   Conditionals Industry
 
 
-//If you have 30 weapons to clean, it will take 60 hours to clean all the weapons. Otherwise if there are less than 30 weapons it will take less than 60 hours to clean the weapons. It takes 2 hours to clean each weapon.
+//If you have 10 weapons to clean, it will take 20 hours to clean all the weapons. It takes 2 hours to clean each weapon unless it needs new parts and then it takes 3 hours for each weapon.
 
 //given number of weapons
 //given number of hours to clean each weapon
@@ -9,5 +9,12 @@
 //result it takes x hours to clean the weapons
 
 
-var numWeapons = 15;
-var hCleanPerWea = 2;
+var numWeapons = 10;   //total number of weapons
+var hCleanWParts = 3;  //number of hours to clean if parts are needed
+
+if(numWeapons == 10){
+    //if there are 10 weapons it will take 20 hours to clean them
+    console.log ("It will take x hours to clean the weapons!");
+}else{
+
+}
