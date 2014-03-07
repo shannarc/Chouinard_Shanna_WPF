3459481ab6b0d2fc8ed71165dc2662769cb316ec
shanna chouinard <shannarc@gmail.com>
2014-03-06 22:22:05 -0500

    Erased previous work and commiting FLOWCHART

--- Chouinard_Shanna_WPF/Associating JS/Associating JS/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Associating JS/js/script.js
@@ -1,11 +1,3 @@
-//Conditional Logic - Ternary Operators
-
-var age = 6;
-var book;
-
-//If the child is under 10, they get Green Eggs and Ham otherwise they get the Time Machine
-book = (age < 10) ? "Green Eggs and Ham" : "The Time Machine";
-console.log(book);
 
 
 
--- Chouinard_Shanna_WPF/Associating JS/Conditionals_Wacky/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Conditionals_Wacky/js/script.js
@@ -1,7 +1,1 @@
-/**
- * Created with JetBrains WebStorm.
- * User: ANewLife
- * Date: 2/24/14
- * Time: 12:47 AM
- * To change this template use File | Settings | File Templates.
- */
+
