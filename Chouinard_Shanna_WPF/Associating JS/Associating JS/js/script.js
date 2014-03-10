792a009b9c0d75bc91e3c3967525b0c77623d474
shanna chouinard <shannarc@gmail.com>
2014-03-10 15:35:14 -0400

    Defining Variables

--- Chouinard_Shanna_WPF/Associating JS/Associating JS/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Associating JS/js/script.js
@@ -1,6 +1,11 @@
-//Javascript
+//Defining Variables
 
-var age;
+var age; //declaration
+age = 38; //definition
+
+//storage container = value
+
+var yearOfBirth = 1978; //declaring and defining on the same line
 
 
 
