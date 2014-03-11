7a4088894314ca7de29b4f28852f111d885702e0
shanna chouinard <shannarc@gmail.com>
2014-03-10 22:31:30 -0400

    Expressions

--- Chouinard_Shanna_WPF/Associating JS/Associating JS/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Associating JS/js/script.js
@@ -1,14 +1,10 @@
-//Arrays
+//Expressions
 
-var muppetNames = ["kermit", "Miss Piggy", "Gonzo", "Rizzo"]; //0,1,2,3
+var a = 2;//sets up pur variable a and defines it with 2
+var b;
+b = a + 3; //add 3 to a
+console.log(b)
 
-console.log(muppetNames[1]);
-
-muppetNames[1]= "Rizzo"; //change index #1 to Rizzo
-muppetNames[2]= "Fozzy";
-
-var num = 1;
-console.log(muppetNames[num]); //lets me see what is in index #1
 
 
 
