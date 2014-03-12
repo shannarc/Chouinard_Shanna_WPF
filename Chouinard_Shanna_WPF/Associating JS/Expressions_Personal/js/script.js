f9c2aa93d71d20b92d74981f31549d2af4e032ac
shanna chouinard <shannarc@gmail.com>
2014-03-12 14:00:19 -0400

    Expressions Personal (Givens)

--- Chouinard_Shanna_WPF/Associating JS/Expressions_Personal/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Expressions_Personal/js/script.js
@@ -1,3 +1,7 @@
 //Shanna Chouinard  12 March 2014  Expressions Personal
 
 //Determine the average amount of minutes it takes to fold 5 loads of laundry.
+
+//Givens
+
+var minsToFold [10, 8, 7, 12, 14];
