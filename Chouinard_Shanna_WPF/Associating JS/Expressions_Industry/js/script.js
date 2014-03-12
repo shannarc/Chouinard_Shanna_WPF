dafe487e3b5efdc905f8d02503c67fbb22098a59
shanna chouinard <shannarc@gmail.com>
2014-03-12 13:49:28 -0400

    Expressions Industry (Print out to console)

--- Chouinard_Shanna_WPF/Associating JS/Expressions_Industry/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Expressions_Industry/js/script.js
@@ -7,5 +7,5 @@ var weapCleanPerH = 10; //10 weapons are cleaned every hour
 
 var total = hoursDay * weapCleanPerH; //total number of weapons cleaned within an 8 hour work day is calculated by multiplying the total number of hours worked with the number of weapons cleaned every hour
 
-
+console.log(total); //Print out the total to the console
 
