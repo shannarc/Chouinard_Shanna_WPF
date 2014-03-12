677653ee3fcc9d2417f52b5e457be4a62c465f2c
shanna chouinard <shannarc@gmail.com>
2014-03-11 22:24:06 -0400

    ??? Dog Years

--- Chouinard_Shanna_WPF/Associating JS/Expressions_Worksheet/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Expressions_Worksheet/js/script.js
@@ -1,7 +1,10 @@
-/**
- * Created with JetBrains WebStorm.
- * User: ANewLife
- * Date: 3/11/14
- * Time: 9:50 PM
- * To change this template use File | Settings | File Templates.
- */
+//Shanna Chouinard   11 March 2014   Expressions Worksheet
+
+//Dog Years
+//Dogs age 7 times faster than humans so a dog that is 1 year old in human years is 7 years old in "dog years."
+//Calculate how old Sparky the pit bull is in "dog years" based on his actual age.
+
+var humanAge = 2;   //Sparky's age in human years'
+var sparkysAge = 7 * humanAge; //Sparky's age in dog years multiplied by human age
+
+console.log(sparkysAge); //print out Sparky's age in dog years
