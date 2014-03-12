256527de920b1444ff7056de0b74a3862e009550
shanna chouinard <shannarc@gmail.com>
2014-03-12 14:37:55 -0400

    Expressions Wacky (Variable used to calculate whole name)

--- Chouinard_Shanna_WPF/Associating JS/Expressions_Wacky/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Expressions_Wacky/js/script.js
@@ -6,3 +6,5 @@
 //Given
 
 var firstMidLast = ["Shanna", "Renee", "Chouinard"];  // My first middle and last name using a string and booleen.
+
+var wholeName = firstMidLast[0] + firstMidLast[1] + firstMidLast [2];
