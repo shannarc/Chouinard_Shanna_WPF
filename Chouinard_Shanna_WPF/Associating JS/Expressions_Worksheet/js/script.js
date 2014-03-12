8f1dcf256e0acff8ac15c5b30813a971fb6284b0
shanna chouinard <shannarc@gmail.com>
2014-03-11 23:25:49 -0400

    Expressions Worksheet Discount

--- Chouinard_Shanna_WPF/Associating JS/Expressions_Worksheet/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Expressions_Worksheet/js/script.js
@@ -1,9 +1,12 @@
 //Shanna Chouinard   11 March 2014   Expressions Worksheet
 
-//Average Shopping Bill
-//You are budgeting your money for the year and must calculate your average weekly grocery shopping spending over the past five weeks. Store the past five grocery totals as a list in an array. Create an expression that will use the items in the array to calculate the average amount spent on groceries.
+//Discounts
+//Calculate the discounted price for an item. Create an expression that will calculate the discounted price with and without sales tax. (It is acceptable for the result to have more than two digits after the decimal. $345.896 for example.)
 
-var groceryTotals = [100, 110, 208, 95, 129];  // grocery spending aver a 5 week period
+var originalPrice = 100;
+var discount = .10;
+var salesTax = .7;
 
-var average = groceryTotals[0] + groceryTotals[1] + groceryTotals[2] + groceryTotals[3] + groceryTotals[4] / 5;//To calculate the average spending over a 5 week period, you must add up the total amount spent and divide it by the number of weeks
-console.log(average);//Print out the average spending for 5 weeks
+var discountItem = originalPrice - discount * 100;
+var itemWTax = discountItem * salesTax
+console.log(itemWTax);
