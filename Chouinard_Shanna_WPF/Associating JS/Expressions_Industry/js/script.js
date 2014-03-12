fba3fe8c4c861d63fe71bbbacb3d48205a4ad8ff
shanna chouinard <shannarc@gmail.com>
2014-03-12 13:47:10 -0400

    Expressions Industry (Givens)

--- Chouinard_Shanna_WPF/Associating JS/Expressions_Industry/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Expressions_Industry/js/script.js
@@ -2,4 +2,7 @@
 
 //10 weapons are cleaned every hour. Determine how many weapons are cleaned within an 8 hour work day.
 
+var hoursDay = 8; //8 hour work day
+var weapCleanPerH = 10; //10 weapons are cleaned every hour
+
 
