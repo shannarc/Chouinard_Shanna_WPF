e6374259fc00e69fc5d6ed4059cc1df0ae247aa4
shanna chouinard <shannarc@gmail.com>
2014-03-11 23:03:39 -0400

    Expression Worksheet Average Shopping Bill

--- Chouinard_Shanna_WPF/Associating JS/Expressions_Worksheet/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Expressions_Worksheet/js/script.js
@@ -1,11 +1,9 @@
 //Shanna Chouinard   11 March 2014   Expressions Worksheet
 
-//Slice of Pie Part 11
-//At the pizza party Sparky, the host's dog is excited, because he gets the leftover pizza after the slices have been divided up evenly among the guests. Assume guests get whole slices, how many whole slices will Sparky get to feast on?
+//Average Shopping Bill
+//You are budgeting your money for the year and must calculate your average weekly grocery shopping spending over the past five weeks. Store the past five grocery totals as a list in an array. Create an expression that will use the items in the array to calculate the average amount spent on groceries.
 
-var slicesPizza = 8;   //Number of slices per pizza
-var numPeople = 10;    //Number of people at the party
-var numPizzas = 5;     //Number of pizzas ordered
+var groceryTotals = [100, 110, 208, 95, 129];  // grocery spending aver a 5 week period
 
-var remainder = numPizzas * slicesPizza % numPeople  //Sparky gets the remainder of the pizza
-console.log(remainder)//Print out the number of pieces Sparky will get to eat
+var average = groceryTotals[0] + groceryTotals[1] + groceryTotals[2] + groceryTotals[3] + groceryTotals[4] / 5;//To calculate the average spending over a 5 week period, you must add up the total amount spent and divide it by the number of weeks
+console.log(average);//Print out the average spending for 5 weeks
