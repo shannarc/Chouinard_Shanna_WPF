2a0dcaa647a0ace25218a87a42af2e6477b36fcb
shanna chouinard <shannarc@gmail.com>
2014-03-11 22:51:04 -0400

    Expressions Worksheet Slice of Pie part II

--- Chouinard_Shanna_WPF/Associating JS/Expressions_Worksheet/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Expressions_Worksheet/js/script.js
@@ -1,13 +1,11 @@
 //Shanna Chouinard   11 March 2014   Expressions Worksheet
 
-//Slice of Pie Part 1
-//A bunch of students are having a party and somebody ordered pizza. Create an expression that calculates how much pizza each partygoer will get at the party. (Assume all pizzas have the same number of slices and that the person dividing the pizza is really precise so this can be a decimal, like 3.52 slices etc.)
+//Slice of Pie Part 11
+//At the pizza party Sparky, the host's dog is excited, because he gets the leftover pizza after the slices have been divided up evenly among the guests. Assume guests get whole slices, how many whole slices will Sparky get to feast on?
 
 var slicesPizza = 8;   //Number of slices per pizza
 var numPeople = 10;    //Number of people at the party
 var numPizzas = 5;     //Number of pizzas ordered
 
-var slicePerPer = numPeople * numPizzas / slicesPizza; //slices per person is the number of people multiplied by the number of pizzas and divided by the number of slices per pizza
-
-console.log(slicePerPer);  //Print out the number of pieces of pizza per person
-
+var remainder = numPizzas * slicesPizza % numPeople  //Sparky gets the remainder of the pizza
+console.log(remainder)//Print out the number of pieces Sparky will get to eat
