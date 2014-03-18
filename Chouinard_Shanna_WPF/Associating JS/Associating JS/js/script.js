02f54e4219ab2c3aa598ab3bc6dafa0b91527978
shanna chouinard <shannarc@gmail.com>
2014-03-17 22:37:15 -0400

    Conditional Logic - With an Expression

--- Chouinard_Shanna_WPF/Associating JS/Associating JS/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Associating JS/js/script.js
@@ -3,10 +3,14 @@
 
 var kidHeight = 50;
 var minHeight = 48;
-var sneakerLift = 2;
+
+
 //if the child is old enough, print to the console "you can ride"
 //if the kid is over 48 inches in height
-if(kidHeight + sneakerLift > minHeight){
+if(kidHeight > minHeight){
     //code performed of the condition is true
     console.log("You can ride the coaster!");
+}else{
+    //code performed if condition is false
+    console.log("Sorry kid, you've got some growing to do first!");
 }
