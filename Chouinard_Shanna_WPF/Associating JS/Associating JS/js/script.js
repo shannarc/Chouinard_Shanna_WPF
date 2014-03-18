292eeb75e6900f01e1c846cb8c813beba00f80c4
shanna chouinard <shannarc@gmail.com>
2014-03-17 22:28:08 -0400

    Conditional Logic - Relational Expressions

--- Chouinard_Shanna_WPF/Associating JS/Associating JS/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Associating JS/js/script.js
@@ -1,12 +1,11 @@
-// Conditional Logic
+// Conditional Logic - Relational Expressions
 
 
-var oldEnough = false;
-
+var kidHeight = 50;
+var minHeight = 48;
 //if the child is old enough, print to the console "you can ride"
-
-if(oldEnough){
+//if the kid is over 48 inches in height
+if(kidHeight > minHeight){
     //code performed of the condition is true
     console.log("You can ride the coaster!");
 }
-console.log("What comes after");
