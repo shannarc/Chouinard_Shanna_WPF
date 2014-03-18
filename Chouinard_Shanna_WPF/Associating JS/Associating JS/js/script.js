e0180a899d4198c1d3ad0f3c9d9f79a0ac627c9e
shanna chouinard <shannarc@gmail.com>
2014-03-17 22:44:00 -0400

    Conditional Logic - else if

--- Chouinard_Shanna_WPF/Associating JS/Associating JS/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Associating JS/js/script.js
@@ -1,16 +1,19 @@
-// Conditional Logic - With an Expression
+// Conditional Logic - else if
 
 
 var kidHeight = 50;
 var minHeight = 48;
-
+var wParentHeight = 45;
 
 //if the child is old enough, print to the console "you can ride"
 //if the kid is over 48 inches in height
 if(kidHeight > minHeight){
-    //code performed of the condition is true
+    //You can ride!
     console.log("You can ride the coaster!");
+}else if(kidHeight > wParentHeight){
+    //You can ride with a parent present
+    console.log("You can ride, but only with a parent present");
 }else{
-    //code performed if condition is false
-    console.log("Sorry kid, you've got some growing to do first!");
+    //sorry you have growing to do
+    console.log("Sorry kid, you've got some growing to do first!")
 }
