89d70611f247df0ec912801f41cd3d9f2506ccc5
shanna chouinard <shannarc@gmail.com>
2014-03-19 11:49:40 -0400

    Check the Login (console print out) I finally figured it out....

--- Chouinard_Shanna_WPF/Associating JS/Conditional_Worksheet/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Conditional_Worksheet/js/script.js
@@ -3,10 +3,19 @@
 
 //Problem: Make sure the user has the correct username and password. If the username does not match, a specific message for that should be printed to the console. If the password does not match, then a message should appear for that. Only one error message should appear.
 
-var userName = Shanna;       //Username entered by user
-var passWord = Chouinard;    //Password entered by user
-var corUserName = Shanna;    //Correct username
-var corPassWord = Chouinard; //Correct password
+var userName = "Shanna";       //Username entered by user
+var passWord = "Chouinard";    //Password entered by user
+var corUserName = "Shanna";    //Correct username
+var corPassWord = "Chouinard"; //Correct password
 
 
-//I can not figure out how to do this!   Please help me
+if(userName === corUserName && passWord === corPassWord){
+    //if user name is Shanna and password is Chouinard then print out to the console, "Welcome Shanna Chouinard"
+    console.log ("Welcome Shanna Chouinard!");
+}else if(userName != corUserName){
+    //if user name is not correct, print out to the console, "user not found, try again"
+    console.log ("User Not Found, Try Again!");
+}else{
+    //if password is not correct, print out to the console, "password does not match our records"
+    console.log ("Password does not match our records!");
+}
