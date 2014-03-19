3e83a4a0adc91751c1191ccd5d872a2d40f345c0
shanna chouinard <shannarc@gmail.com>
2014-03-19 10:21:54 -0400

    Last Chance for Gas (console print out)

--- Chouinard_Shanna_WPF/Associating JS/Conditional_Worksheet/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Conditional_Worksheet/js/script.js
@@ -6,3 +6,11 @@
 var gasEfficiency = 13   //gas efficiency in mpg
 var gasRemaining = 75    //gauge reading of the gas tank in %
 var gasTankHolds = 16    //car's gas tank capacity in gallons
+
+if (gasTankHolds * gasRemaining / 100 * gasEfficiency > 200){
+    //If its more than 200
+    console.log ("Yes you can make it without stopping for gas!");
+}else{
+    //if its less than 200
+    console.log ("You only have X gallons of gas in your tank, better get gas now while you can!");
+}
