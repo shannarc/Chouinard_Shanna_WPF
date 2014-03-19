2ab6282ce98c00240d8e1b97bf8659c4b2ed7b7b
shanna chouinard <shannarc@gmail.com>
2014-03-19 14:56:56 -0400

    Conditionals Personal (Problem)

--- Chouinard_Shanna_WPF/Associating JS/Conditionals_Personal/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Conditionals_Personal/js/script.js
@@ -1,4 +1,4 @@
 //Shanna Chouinard   19 March 2014
 //Conditionals Personal
 
-
+//Problem:  Will I ride on the Harley today or will I drive the car? If it is windy outside, I will drive the car and if it is below 80 degrees, I will drive the car otherwise I will ride on the Harley!
