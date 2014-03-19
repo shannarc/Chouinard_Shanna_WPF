63a327c52d08c407e47df2d9b1bb0a0e35942098
shanna chouinard <shannarc@gmail.com>
2014-03-19 13:00:39 -0400

    Movie Ticket Price (What the variables are)

--- Chouinard_Shanna_WPF/Associating JS/Conditional_Worksheet/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Conditional_Worksheet/js/script.js
@@ -3,5 +3,5 @@
 
 //Problem: The local movie theatre in town has a ticket price of $12.00 but if you are a senior (55 and older), under 10 you get the discounted price of $7.00. In addition, if a customer is seeing a movie between 3pm and 5pm they can also get the discounted price.
 
-var timeOfMovie = 2;
-var ageOfCust = 14;
+var timeOfMovie = 2; //Time of Movie, assume whole numbers
+var ageOfCust = 14; //Age of the customer
