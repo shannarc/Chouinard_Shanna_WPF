0be4256a8535ce3cd8c9d4e179c8c6c05e7d818d
shanna chouinard <shannarc@gmail.com>
2014-03-19 10:27:32 -0400

    Check the Login (problem)

--- Chouinard_Shanna_WPF/Associating JS/Conditional_Worksheet/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Conditional_Worksheet/js/script.js
@@ -1,2 +1,4 @@
 //Group 2 Multiple Results
 //Check the Login
+
+//Problem: Make sure the user has the correct username and password. If the username does not match, a specific message for that should be printed to the console. If the password does not match, then a message should appear for that. Only one error message should appear.
