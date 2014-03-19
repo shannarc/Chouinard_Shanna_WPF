a225dd1d8160a90e7fd7401018d9143e24c06a7e
shanna chouinard <shannarc@gmail.com>
2014-03-19 13:53:40 -0400

    Conditionals Industry (Problem)

--- Chouinard_Shanna_WPF/Associating JS/Conditionals_Industry/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Conditionals_Industry/js/script.js
@@ -1,2 +1,4 @@
 //Shanna Chouinard   19 March 2014   Conditionals "Industry"
 
+//Problem:  Our work schedule is four 10 hour shifts. We come in at 6:30 and get off at 5:00. If it snows outside, we get off an hour early. What time do we get off?
+
