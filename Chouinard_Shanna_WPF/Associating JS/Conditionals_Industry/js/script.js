378ddc2ba52bfe3d27fc9561d0cf95ef89679e29
shanna chouinard <shannarc@gmail.com>
2014-03-19 14:01:23 -0400

    Conditionals Industry (Meaning of the variables)

--- Chouinard_Shanna_WPF/Associating JS/Conditionals_Industry/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Conditionals_Industry/js/script.js
@@ -1,4 +1,9 @@
 //Shanna Chouinard   19 March 2014   Conditionals "Industry"
 
-//Problem:  Our work schedule is four 10 hour shifts. We come in at 6:30 and get off at 5:00. If it snows outside, we get off an hour early. What time do we get off?
+//Problem:  Our work schedule is four 10 hour shifts. We come in at 6:30 and get off at 5:00. If it snows outside, we get off an hour early. We also get off an o\hour early on Fridays. What time do we get off?
+
+var weather = "snow";      //Is it snowing outside or not?
+var dayOfWeek = "Tuesday"; //What day of the week is it?
+
+
 
