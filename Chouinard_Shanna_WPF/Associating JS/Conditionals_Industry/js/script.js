db7ea0857f133232f9d3cca4f4ef306b0deeb6a6
shanna chouinard <shannarc@gmail.com>
2014-03-19 14:42:30 -0400

    Conditionals Industry (If Statement)

--- Chouinard_Shanna_WPF/Associating JS/Conditionals_Industry/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Conditionals_Industry/js/script.js
@@ -2,8 +2,11 @@
 
 //Problem:  Our work schedule is four 10 hour shifts. We come in at 6:30 and get off at 5:00. If it snows outside, we get off an hour early. We also get off an o\hour early on Fridays. What time do we get off?
 
-var weather = "snow";      //Is it snowing outside or not?
-var dayOfWeek = "Tuesday"; //What day of the week is it?
+var weather = "sunny";      //Is it snowing outside or not?
+var dayOfWeek = "Friday";  //What day of the week is it?
 
-
+if(weather === "snow" ^ dayOfWeek === "Friday"){
+    //if it is snowing outside or it is Friday, print out to the console, "you will be getting off at 4:00 today"
+    console.log("You will be getting off at 4:00 today!");
+}
 
