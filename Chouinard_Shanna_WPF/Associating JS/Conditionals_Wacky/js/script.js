711327165fde3701381fd3c06e2855ed6de84376
shanna chouinard <shannarc@gmail.com>
2014-03-19 15:29:37 -0400

    Conditionals Wacky (Meaning of Variables)

--- Chouinard_Shanna_WPF/Associating JS/Conditionals_Wacky/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Conditionals_Wacky/js/script.js
@@ -3,6 +3,8 @@
 
 //Problem:  If I am over the maximum weight, I can not jump from the plane
 
-var weight = 162;
-var maxWeight = 230;
+var weight = 162;   //My actual weight
+var maxWeight = 230;  //Maximum aloud weight to jump from plane
+
+
 
