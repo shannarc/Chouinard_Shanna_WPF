7604fd563d9a657431e1d19db90e8712c00b7d6a
shanna chouinard <shannarc@gmail.com>
2014-03-19 10:23:52 -0400

    Check the Login

--- Chouinard_Shanna_WPF/Associating JS/Conditional_Worksheet/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Conditional_Worksheet/js/script.js
@@ -1,16 +1,2 @@
-//Group 1 Expressions with Conditionals
-//Last Chance for Gas
-
-//Problem: A driver has to determine if they can make it across the desert with their current fuel. They are about to pass the last gas station for the next 200 miles and they need to determine if they should stop now for gas or not.
-
-var gasEfficiency = 13   //gas efficiency in mpg
-var gasRemaining = 75    //gauge reading of the gas tank in %
-var gasTankHolds = 16    //car's gas tank capacity in gallons
-
-if (gasTankHolds * gasRemaining / 100 * gasEfficiency > 200){
-    //If its more than 200
-    console.log ("Yes you can make it without stopping for gas!");
-}else{
-    //if its less than 200
-    console.log ("You only have X gallons of gas in your tank, better get gas now while you can!");
-}
+//Group 2 Multiple Results
+//Check the Login
