bcc87d0749b6cfe9b469238af44231514de96adc
shanna chouinard <shannarc@gmail.com>
2014-03-19 09:53:07 -0400

    Last Chance for Gas (What the variables are)

--- Chouinard_Shanna_WPF/Associating JS/Conditional_Worksheet/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Conditional_Worksheet/js/script.js
@@ -3,6 +3,6 @@
 
 //Problem: A driver has to determine if they can make it across the desert with their current fuel. They are about to pass the last gas station for the next 200 miles and they need to determine if they should stop now for gas or not.
 
-var gasEfficiency = 13
-var gasRemaining = 75
-var gasTankHolds = 16
+var gasEfficiency = 13   //gas efficiency in mpg
+var gasRemaining = 75    //gauge reading of the gas tank in %
+var gasTankHolds = 16    //car's gas tank capacity in gallons
