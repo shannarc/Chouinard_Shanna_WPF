f204ea22fd6ef55f254246db15b0e2c7b95141fd
shanna chouinard <shannarc@gmail.com>
2014-03-19 09:46:04 -0400

    Last Chance for Gas (Problem)

--- Chouinard_Shanna_WPF/Associating JS/Conditional_Worksheet/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Conditional_Worksheet/js/script.js
@@ -1,3 +1,4 @@
 //Group 1 Expressions with Conditionals
 //Last Chance for Gas
 
+//Problem: A driver has to determine if they can make it across the desert with their current fuel. They are about to pass the last gas station for the next 200 miles and they need to determine if they should stop now for gas or not.
