02c74e0ef0bd27a9a834933998da3f40f6624b09
shanna chouinard <shannarc@gmail.com>
2014-03-19 15:35:23 -0400

    Conditionals Wacky (Else Statement)

--- Chouinard_Shanna_WPF/Associating JS/Conditionals_Wacky/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Conditionals_Wacky/js/script.js
@@ -3,11 +3,14 @@
 
 //Problem:  If I am over the maximum weight, I can not jump from the plane
 
-var weight = 162;   //My actual weight
+var weight = 280;   //My actual weight
 var maxWeight = 230;  //Maximum aloud weight to jump from plane
 
 if(weight < maxWeight){
     //If my weight is less than the maximum weight aloud to jump off the plane, print out to the console, "I get to JUMP!"
     console.log ("I get to JUMP!");
+}else{
+    //If my weight is more than the maximum weight aloud to jump from the plane, print out to the console, "I am soooooo sorry, you need to lose some weight!"
+    console.log ("I am soooooo sorry, you need to lose weight!");
 }
 
