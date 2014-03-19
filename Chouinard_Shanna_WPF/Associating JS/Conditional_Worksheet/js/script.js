a05eadc70408d5822aff02d6617da8f6177b4052
shanna chouinard <shannarc@gmail.com>
2014-03-19 12:56:52 -0400

    Movie Ticket Price (Problem)

--- Chouinard_Shanna_WPF/Associating JS/Conditional_Worksheet/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Conditional_Worksheet/js/script.js
@@ -1,3 +1,5 @@
 //Group 3 Multiple Conditions
 //Movie Ticket Price
 
+//Problem: The local movie theatre in town has a ticket price of $12.00 but if you are a senior (55 and older), under 10 you get the discounted price of $7.00. In addition, if a customer is seeing a movie between 3pm and 5pm they can also get the discounted price.
+
