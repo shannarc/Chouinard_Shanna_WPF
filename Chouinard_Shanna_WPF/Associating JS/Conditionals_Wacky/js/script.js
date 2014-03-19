8c19ab4d8749f89e022e9ed599c1698d58fc328a
shanna chouinard <shannarc@gmail.com>
2014-03-19 15:32:35 -0400

    Conditionals Wacky (If Statement)

--- Chouinard_Shanna_WPF/Associating JS/Conditionals_Wacky/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Conditionals_Wacky/js/script.js
@@ -6,5 +6,8 @@
 var weight = 162;   //My actual weight
 var maxWeight = 230;  //Maximum aloud weight to jump from plane
 
-
+if(weight < maxWeight){
+    //If my weight is less than the maximum weight aloud to jump off the plane, print out to the console, "I get to JUMP!"
+    console.log ("I get to JUMP!");
+}
 
