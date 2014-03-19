02ead7f56704427a1bc5fff53ae10ab0489489f5
shanna chouinard <shannarc@gmail.com>
2014-03-19 10:31:38 -0400

    Check the Login (what the variables are)

--- Chouinard_Shanna_WPF/Associating JS/Conditional_Worksheet/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Conditional_Worksheet/js/script.js
@@ -3,7 +3,7 @@
 
 //Problem: Make sure the user has the correct username and password. If the username does not match, a specific message for that should be printed to the console. If the password does not match, then a message should appear for that. Only one error message should appear.
 
-var userName = Shanna
-var passWord = Chouinard
-var corUserName = Shanna
-var corpassWord = Chouinard
+var userName = Shanna   //Username entered by user
+var passWord = Chouinard  //Password entered by user
+var corUserName = Shanna  //Correct username
+var corPassWord = Chouinard  //Correct password
