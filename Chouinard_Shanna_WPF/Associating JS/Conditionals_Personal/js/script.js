27bebc2e95890441ab584bccc955fe6d4151ae69
shanna chouinard <shannarc@gmail.com>
2014-03-19 15:15:45 -0400

    Cinditionals Personal (Else If Statement)

--- Chouinard_Shanna_WPF/Associating JS/Conditionals_Personal/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Conditionals_Personal/js/script.js
@@ -1,12 +1,15 @@
 //Shanna Chouinard   19 March 2014
 //Conditionals Personal
 
-//Problem:  Will I ride on the Harley today or will I drive the car? If it is windy or rainy outside, I will drive the car and if it is below 80 degrees, I will drive the car otherwise I will ride on the Harley!
+//Problem:  Will I ride on the Harley today or will I drive the car? If it is windy, rainy or below 80 degrees, I will drive the car.  otherwise I will ride on the Harley!
 
 var weather = "windy";  //how is the weather, ie windy, rainy, snowy
-var temp = 82;         //what is the temperature using whole numbers
+var temp = 79;         //what is the temperature using whole numbers
 
 if(weather === "windy" ^ weather === "rainy"){
     //If it is windy or rainy, print out to the console, "I will drive the car today!"
     console.log ("I will drive the car today!");
+}else if(weather = "Sunny" && temp < 80){
+    //If it is sunny but the temperature is less than 80 degrees, print out to the console, "It is not warm enough to ride the Harley!"
+    console.log ("It is not warm enough to ride the Harley!");
 }
