1e4187f84b13153e25bea25a2af2e0e735761f64
shanna chouinard <shannarc@gmail.com>
2014-03-19 15:01:48 -0400

    Conditionals Personal (Meaning of Variables)

--- Chouinard_Shanna_WPF/Associating JS/Conditionals_Personal/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Conditionals_Personal/js/script.js
@@ -3,5 +3,5 @@
 
 //Problem:  Will I ride on the Harley today or will I drive the car? If it is windy or rainy outside, I will drive the car and if it is below 80 degrees, I will drive the car otherwise I will ride on the Harley!
 
-var weather = "windy";
-var temp = 82;
+var weather = "windy";  //how is the weather, ie windy, rainy, snowy
+var temp = 82;         //what is the temperature using whole numbers
