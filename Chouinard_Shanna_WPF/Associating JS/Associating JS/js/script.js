57648531cb95186a1fdaf8005829e32856fc2bbd
shanna chouinard <shannarc@gmail.com>
2014-03-26 10:27:05 -0400

    Functions (Variable Scope)

--- Chouinard_Shanna_WPF/Associating JS/Associating JS/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Associating JS/js/script.js
@@ -1,16 +1,13 @@
-//Functions  Function Execution
+//Functions  Variable Scope
 
-function autoMsg(){
-    console.log("Hello World");
-}
+var width = 5;
 
 function calcArea(){
     var width = 20;
     var height = 30;
     var area = width * height;
-    console.log (area);
+    //console.log (area);
 }
 
 calcArea();
-calcArea();
-calcArea();
+console.log(width);
