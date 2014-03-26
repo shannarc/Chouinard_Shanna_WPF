e1d4154e4c3c82723ca7b4ac8b09b05d7978d988
shanna chouinard <shannarc@gmail.com>
2014-03-26 08:59:38 -0400

    Functions (Basic Structure)

--- Chouinard_Shanna_WPF/Associating JS/Associating JS/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Associating JS/js/script.js
@@ -1,19 +1,12 @@
-// Conditional Logic - else if
+//Functions  Basic Structure
 
+function autoMsg(){
+    console.log("Hello World");
+}
 
-var kidHeight = 50;
-var minHeight = 48;
-var wParentHeight = 45;
-
-//if the child is old enough, print to the console "you can ride"
-//if the kid is over 48 inches in height
-if(kidHeight > minHeight){
-    //You can ride!
-    console.log("You can ride the coaster!");
-}else if(kidHeight > wParentHeight){
-    //You can ride with a parent present
-    console.log("You can ride, but only with a parent present");
-}else{
-    //sorry you have growing to do
-    console.log("Sorry kid, you've got some growing to do first!")
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log (area);
 }
