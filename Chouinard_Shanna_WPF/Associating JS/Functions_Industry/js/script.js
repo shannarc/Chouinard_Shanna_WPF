97594158c2029727b4817528704e3755e50dfa11
shanna chouinard <shannarc@gmail.com>
2014-03-27 12:48:16 -0400

    Functions Industry (Problem)

--- Chouinard_Shanna_WPF/Associating JS/Functions_Industry/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Functions_Industry/js/script.js
@@ -1,4 +1,7 @@
 //Shanna Chouinard   27 March 2014   Functions Industry
 
+//How many pieces of equipment are repaired per 8 hour day if there is 1 piece of equipment repaired per hour?
 
-function total ()
+
+function total (){  //Function Name
+
