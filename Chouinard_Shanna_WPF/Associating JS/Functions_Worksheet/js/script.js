3cfa4bb137037e27e9ea58798483471bf2699679
shanna chouinard <shannarc@gmail.com>
2014-03-26 23:32:25 -0400

    Functions Worksheet (Circumference)

--- Chouinard_Shanna_WPF/Associating JS/Functions_Worksheet/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Functions_Worksheet/js/script.js
@@ -1,1 +1,14 @@
+//Shanna Chouinard   26 March 2014  Functions Worksheet
 
+
+
+//Calculate the circumference of a circle
+
+function calcCirc(){  //functionName
+    var radius = 5;   //radius of the circle
+    var pie = 3.14;   //variable pie
+    var calcCirc = 2 * pie * radius; //how to calculate the circumference of a circle
+    console.log("The circumference of the circle is " + calcCirc + "!"); //Print out to console
+
+}
+calcCirc();   //call
