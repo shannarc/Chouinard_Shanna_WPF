70683e989cd30d8ee744a8d2f27482b4e5ef6662
shanna chouinard <shannarc@gmail.com>
2014-03-27 00:01:38 -0400

    Functions (Files added to wacky, personal and industry folders)

---
+++ Chouinard_Shanna_WPF/Associating JS/Functions_Industry/js/script.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: ANewLife
+ * Date: 3/26/14
+ * Time: 11:59 PM
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ Chouinard_Shanna_WPF/Associating JS/Functions_Personal/js/script.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: ANewLife
+ * Date: 3/27/14
+ * Time: 12:00 AM
+ * To change this template use File | Settings | File Templates.
+ */
