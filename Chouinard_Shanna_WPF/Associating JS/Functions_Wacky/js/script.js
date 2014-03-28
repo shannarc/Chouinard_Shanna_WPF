2c7371652481324e3a27219c9e0bf3190ee0b61e
shanna chouinard <shannarc@gmail.com>
2014-03-27 22:26:45 -0400

    Functions Wacky (Assignment Name)

--- Chouinard_Shanna_WPF/Associating JS/Functions_Wacky/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Functions_Wacky/js/script.js
@@ -1,16 +1,5 @@
-Shanna Chouinard   27 March 2014
+//Shanna Chouinard   27 March 2014   Functions Wacky
 
 
 
 
-//Combine my first middle and last name using a string and booleen.
-
-//Given
-
-var firstMidLast = ["Shanna", "Renee", "Chouinard"];  // My first middle and last name using a string and booleen.
-
-var wholeName = firstMidLast[0] + " " +firstMidLast[1] + " " + firstMidLast [2];  // add the strings within the booleen together to calculate the whole name
-// I went back in and added the quotation marks within the calculation to add a space between the first and middle name as well as a space between the middle and last name.
-
-console.log(wholeName);   //Console printout ShannaReneeChouinard
-// New console printout Shanna Renee Chouinard
