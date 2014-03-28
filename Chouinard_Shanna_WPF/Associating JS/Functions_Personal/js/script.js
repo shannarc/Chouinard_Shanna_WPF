68697ef0c86b675ee176cf99b0b01ef4e53d2ab0
shanna chouinard <shannarc@gmail.com>
2014-03-27 22:13:19 -0400

    Functions Personal (Variables and Explanations)

--- Chouinard_Shanna_WPF/Associating JS/Functions_Personal/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Functions_Personal/js/script.js
@@ -5,3 +5,9 @@
 
 //Determine the average amount of minutes it takes to feed 4 dogs.
 
+
+
+var minsToFeed =[15, 10, 9, 14, 13]; //Amount of minutes it took to feed 4 dogs.
+
+var totalMins = minsToFeed[0] + minsToFold[1] + minsToFold[2] + minsToFold[3] + minsToFold[4] / 5;   //Calculation of the total minutes it took to feed 4 dogs. Now divide the total by 5 to get the average amount of minutes to feed the dogs.
+
