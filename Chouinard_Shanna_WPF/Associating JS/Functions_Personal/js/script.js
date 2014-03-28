c5bd03b74c2e5cdd3ebc29d5a3d5739aa496c695
shanna chouinard <shannarc@gmail.com>
2014-03-27 22:14:41 -0400

    Functions Personal (console printout)

--- Chouinard_Shanna_WPF/Associating JS/Functions_Personal/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Functions_Personal/js/script.js
@@ -11,3 +11,4 @@ var minsToFeed =[15, 10, 9, 14, 13]; //Amount of minutes it took to feed 4 dogs.
 
 var totalMins = minsToFeed[0] + minsToFold[1] + minsToFold[2] + minsToFold[3] + minsToFold[4] / 5;   //Calculation of the total minutes it took to feed 4 dogs. Now divide the total by 5 to get the average amount of minutes to feed the dogs.
 
+console.log(totalMins);  // print out the average minutes it takes to feed all four dogs
