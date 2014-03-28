5b36bbee45c938a71d06c724fa810fa10f13906a
shanna chouinard <shannarc@gmail.com>
2014-03-27 22:19:29 -0400

    Functions Personal (adjustments to variable)

--- Chouinard_Shanna_WPF/Associating JS/Functions_Personal/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Functions_Personal/js/script.js
@@ -9,6 +9,8 @@
 
 var minsToFeed =[15, 10, 9, 14, 13]; //Amount of minutes it took to feed 4 dogs.
 
-var totalMins = minsToFeed[0] + minsToFold[1] + minsToFold[2] + minsToFold[3] + minsToFold[4] / 5;   //Calculation of the total minutes it took to feed 4 dogs. Now divide the total by 5 to get the average amount of minutes to feed the dogs.
+var totalMins = minsToFeed[0] + minsToFeed[1] + minsToFeed[2] + minsToFeed[3] + minsToFeed[4] / 5;   //Calculation of the total minutes it took to feed 4 dogs. Now divide the total by 5 to get the average amount of minutes to feed the dogs.
 
 console.log(totalMins);  // print out the average minutes it takes to feed all four dogs
+}
+
