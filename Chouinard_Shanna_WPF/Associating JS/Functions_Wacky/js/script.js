c638f0b9623726820c62a65525a82b15aed6b746
shanna chouinard <shannarc@gmail.com>
2014-03-27 22:44:11 -0400

    Functions Wacky (Function Name)

--- Chouinard_Shanna_WPF/Associating JS/Functions_Wacky/js/script.js
+++ Chouinard_Shanna_WPF/Associating JS/Functions_Wacky/js/script.js
@@ -5,12 +5,12 @@
 
 //Combine the four different words together to make my Savior Statement using a string and booleen.
 
-
+function myState {
 
 var mySavior = ["JESUS", "Is", "My", "Savior"];  // My Savior statement using a string and booleen.
 
 var statement = mySavior[0] + " " +mySavior[1] + " " + mySavior[2] + " " + mySavior[3];  // add the strings within the booleen together to calculate the whole statement
 
-console.log(statement);
-
+console.log(statement); //Print out to console
+}
 
